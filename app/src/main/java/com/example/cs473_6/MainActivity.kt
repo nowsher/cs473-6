f773353fb6f74888f87ac978a44448f53a6927ef
nowsher <alinowsher@yahoo.com>
2022-10-21 09:28:17 -0500

    v8

--- app/src/main/java/com/example/cs473_6/MainActivity.kt
+++ app/src/main/java/com/example/cs473_6/MainActivity.kt
@@ -1,24 +1,15 @@
 package com.example.cs473_6
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.text.Html
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
 import android.widget.Toast
-import androidx.core.text.bold
-import androidx.core.text.buildSpannedString
-import androidx.core.text.italic
-import androidx.core.text.underline
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_home.*
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private lateinit var fragManager: FragmentManager
@@ -54,17 +45,84 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Toast.makeText(
-            applicationContext,
-            item.title.toString(),
-            Toast.LENGTH_LONG
-        ).show()
+
+        when(item.itemId){
+            R.id.mail ->{
+
+                //Email
+                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                    type = "*/*"    //"text/plain"
+                    data = Uri.parse("mailto:mohammad.ali@miu.edu")
+//                    putExtra(Intent.EXTRA_SUBJECT, "Well wishes")
+//                    putExtra(Intent.EXTRA_TEXT, "Dear Sir/Madam,")
+                }
+                startActivity(intent)
+
+                return true
+            }
+            R.id.linkedin ->{
+                //Linkedin
+//                val uri = Uri.parse("https://www.linkedin.com/feed/")
+//                val it = Intent(Intent.ACTION_VIEW, uri)
+//                startActivity(it)
+
+                val sendIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type = "text/plain"
+                    setPackage("com.linkedin.android")
+                }
+                if (sendIntent != null) {
+                    startActivity(sendIntent)
+                }
+
+                return true
+            }
+            R.id.whatsapp ->{
+                //Whatsapp
+                val sendIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type = "text/plain"
+                    setPackage("com.whatsapp")
+                }
+                if (sendIntent != null) {
+                    startActivity(sendIntent)
+                }
+                else{
+                    Toast.makeText(this, "Application not available",Toast.LENGTH_LONG)
+                }
+
+                return true
+            }
+            R.id.gmail ->{
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SENDTO
+                    type = "text/plain"
+                    data = Uri.parse("mailto:mohammad.ali@miu.edu")
+//                    setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail")
+                    setPackage("com.google.android.gm")
+                    putExtra(Intent.EXTRA_SUBJECT, "Well wishes")
+                    putExtra(Intent.EXTRA_TEXT, "Dear Sir/Madam,")
+                }
+                if (intent != null) {
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this, "Application not available",Toast.LENGTH_LONG)
+                }
+                return true
+            }
+            else ->{
+                return true
+            }
+
+        }
 
         return super.onOptionsItemSelected(item)
     }
